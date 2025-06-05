a2e3d379e670d2117babea1dfb2604f43d433b37
Vamsi8063 <mulakavamsikrishnareddy@gmail.com>
2025-06-05 11:00:42 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId =8063
+let accountEmail="mulaka@gmail.com"
+var accountPassword="7569"
+accountCity="Bengaluru"
+
+
+accountEmail="reddy@gmail.com"
+accountPassword="9676"
+accountPassword="inkollu"
+console.log(accountEmail)
+console.table([accountId, accountEmail, accountPassword, accountCity])
